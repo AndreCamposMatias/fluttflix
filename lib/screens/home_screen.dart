80cef93de9ddd11d0f895ae80133f65e9f99b54a
andrematias <andre.cmatias@gmail.com>
2021-07-16 12:59:51 +0100

    Fade App Bar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -7,6 +7,26 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  //tell Dart that you are going to initialize the variable at some other point before using it
+  late ScrollController _scrollController;
+  double _scrollOffset = 0.0;
+
+  @override
+  void initState() {
+    _scrollController = ScrollController()..addListener(() {
+      setState(() {
+        _scrollOffset = _scrollController.offset;
+      });
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     final Size screenSize = MediaQuery.of(context).size;
@@ -18,8 +38,19 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
       appBar: PreferredSize(
         preferredSize: Size(screenSize.width, 50.0),
-        child: CustomAppBar(),
+        child: CustomAppBar(
+          scrollOffset: _scrollOffset,
+        ),
       ),
+      body: CustomScrollView(controller: _scrollController,
+      slivers: [
+        SliverToBoxAdapter(
+          child: Container(
+            height: 1000.0,
+            color: Colors.blue,
+          ),
+        ),
+      ],),
     );
   }
 }

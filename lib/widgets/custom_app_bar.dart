18c0ac054ec1ba65cefb695aac1f600cf0457cc3
andrematias <andre.cmatias@gmail.com>
2021-07-16 11:58:54 +0100

    Custom App Bar

---
+++ lib/widgets/custom_app_bar.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:fluttflix/assets.dart';
+
+class CustomAppBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(
+        vertical: 10.0,
+        horizontal: 24.0,
+      ),
+      color: Colors.orange,
+      child: SafeArea(
+        child: Row(
+          children: [
+            Image.asset(Assets.netflixLogo0),
+          ],
+        ),
+      ),
+    );
+  }
+}

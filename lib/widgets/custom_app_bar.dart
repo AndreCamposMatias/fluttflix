2152deffc2bc6469451f6a1d70545b9f31b30256
andrematias <andre.cmatias@gmail.com>
2021-07-16 12:22:27 +0100

    App Bar Buttons

--- lib/widgets/custom_app_bar.dart
+++ lib/widgets/custom_app_bar.dart
@@ -14,9 +14,57 @@ class CustomAppBar extends StatelessWidget {
         child: Row(
           children: [
             Image.asset(Assets.netflixLogo0),
+            const SizedBox(width: 12.0),
+            Expanded(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  _AppBarButton(
+                    title: 'TV Shows',
+                    onTap: () => print('Tv Shows'),
+                  ),
+                  _AppBarButton(
+                    title: 'Movies',
+                    onTap: () => print('Movies'),
+                  ),
+                  _AppBarButton(
+                    title: 'My List',
+                    onTap: () => print('My List'),
+                  ),
+                ],
+              ),
+            )
           ],
         ),
       ),
     );
   }
 }
+
+
+class _AppBarButton extends StatelessWidget {
+  final String title;
+  final Function()? onTap;
+
+  const _AppBarButton({
+    Key? key,
+    required this.title,
+    required this.onTap
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Text(
+        title,
+        style: const TextStyle(
+          color: Colors.white,
+          fontSize: 16.0,
+          fontWeight: FontWeight.w600,
+        ),
+      ),
+    );
+  }
+}
+
